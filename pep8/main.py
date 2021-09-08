dcc23e4aac48f469c5424be763209d0a0ce8cd71
Fernando <FernandoVasconcelos>
2021-09-08 16:26:53 -0300

    PEP8 aula2

--- pep8/main.py
+++ pep8/main.py
@@ -1,8 +1,9 @@
 from fila_normal import Fila_normal
-
-newFila = Fila_normal()
-newFila.atualiza_fila()
-newFila.atualiza_fila()
-newFila.atualiza_fila()
-print(newFila.chama_cliente(5))
-print(newFila.chama_cliente(10))
+from fila_prioritaria import FilaPrioritaria
+fila_teste = FilaPrioritaria()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+print(f"{fila_teste.chama_cliente(10)}")
+print(f"{fila_teste.chama_cliente(1)}")
+print(f"{fila_teste.estatistica('10/01/1993', 198, 'detail')}")
